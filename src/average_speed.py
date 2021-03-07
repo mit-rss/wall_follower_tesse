39b0bdf428730af624c01b2f050ef7bd088edfcd
tanika <tanika@mit.edu>
2021-03-06 23:41:05 -0500

    fixing speed logger to work with launch file

--- src/average_speed.py
+++ src/average_speed.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python2
 
-import sys
 import numpy as np
 
 if __name__ == "__main__":
-    speed_log_filename = sys.argv[1] if len(sys.argv) > 1 else "speed_log.txt"
+    speed_log_filename = "/home/racecar/.ros/speed_log.txt"
 
     xyz_speeds = np.loadtxt(speed_log_filename)
     average_speeds = np.mean(a=xyz_speeds,
--- src/record_speeds.py
+++ src/record_speeds.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python2
 
-import sys
 import numpy as np
 import rospy
 from nav_msgs.msg import Odometry
 
 class RecordSpeeds:
     # topic for odometry information (pose and twist)
-    ODOM_TOPIC = "/tesse/odom" #eventually: ("wall_follower_tesse/odometry_topic")
+    ODOM_TOPIC = rospy.get_param("record_speeds/odometry_topic")
 
-    def __init__(self, speed_log_filename):
+    def __init__(self):
         # subscribe to odometry messages
         rospy.Subscriber(
                 self.ODOM_TOPIC,
@@ -17,7 +16,7 @@ class RecordSpeeds:
                 self.odom_callback)
 
         # log will contain all speed data collected from odometry messages
-        self.log_file = speed_log_filename
+        self.log_file = "speed_log.txt"
         self.speed_log = None
 
     def odom_callback(self, odom_data):
@@ -29,15 +28,12 @@ class RecordSpeeds:
             self.speed_log = linear_velocity
         else:
             self.speed_log = np.vstack((self.speed_log,
-                                       linear_velocity))
+                                        linear_velocity))
 
         np.savetxt(fname=self.log_file,
                    X=self.speed_log)
 
 if __name__ == "__main__":
     rospy.init_node("record_speeds")
-
-    speed_log_filename = sys.argv[1] if len(sys.argv) > 1 else "speed_log.txt"
-    speed_recorder = RecordSpeeds(speed_log_filename=speed_log_filename)
-
+    speed_recorder = RecordSpeeds()
     rospy.spin()
